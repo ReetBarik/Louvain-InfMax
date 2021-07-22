5e4e304c1d0031eb7629dc0efe559080f1ef78f7
ReetBarik <reetbarik94@gmail.com>
2021-07-21 22:45:21 -0700

    Seed vs Comm size (sorted comm size): BerkStan. Updated ppt

--- CommInvestigate/CommInvestigate.py
+++ CommInvestigate/CommInvestigate.py
@@ -14,7 +14,7 @@ from collections import Counter
 import numpy as np
 import json
 
-input = 'soc-Epinions1.txt'
+input = 'BerkStan.txt'
 
 def read_integers(filename):
     with open(filename) as f:
@@ -24,29 +24,32 @@ comm = read_integers('../NewExpts/Grappolo/ClusterAssignments/' + input + '_clus
     
 comm_size = Counter(comm)
 
-seedfile = 'Seeds/' + input + '_imm_100.json'
+comm_size = dict(sorted(comm_size.items(), key=lambda item: item[1]))
+
+seedfile = 'Seeds/' + input + '_grappolo_100.json'
 
 with open(seedfile) as json_file:
     data = json.load(json_file)
     
 seeds = list(data[0]['Seeds']) 
 
-seed_comm_size = np.zeros(len(comm_size))
+seed_comm = np.zeros(len(comm_size))
 
 for s in seeds:
     c = comm[s]
-    seed_comm_size[c] += 1
+    seed_comm[c] += 1
     
     
-global_comm_size = np.zeros(len(comm_size))
+global_comm_size = []
+seed_comm_size = []
 
-for i in range(len(comm_size)):
-    global_comm_size[i] += comm_size[i]
+for i in comm_size:
+    global_comm_size.append(comm_size[i])
+    seed_comm_size.append(seed_comm[i])
 
-
-seed_comm_size = seed_comm_size / 100
-
-global_comm_size = global_comm_size / len(comm)
+#seed_comm_size = seed_comm_size / 100
+#
+#global_comm_size = global_comm_size / len(comm)
 
 df = pd.DataFrame({'seed': seed_comm_size,'comm': global_comm_size})
 
@@ -54,16 +57,18 @@ df.to_csv(input + '.csv', sep = ',', index = False)
 
 fig, ax = plt.subplots()
 
-ax.plot(seed_comm_size, color = 'red', label = 'Seeds (IMM)')
+ax.plot(global_comm_size, marker = 'o', alpha = 0.4, markersize = 3, color = 'blue')
+ax2 = ax.twinx()
+ax2.plot(seed_comm_size, marker = 'o', alpha = 0.2, markersize = 2, color = 'red')
 
-ax.plot(global_comm_size, color = 'blue', alpha = 0.6, label = 'Communities')
+#ax.legend()
+fig.text(0.3, 0.90, 'Community Size (left y axis)', c = 'blue', ha='center')
+fig.text(0.5, 0.96, 'BerkStan: n = 685K, m = 7.6M, mod = 0.94, #comm = ' + str(len(global_comm_size)), ha='center')
+fig.text(0.5, 0.90, 'vs', ha='center')
+fig.text(0.725, 0.90, '#L-IMM seeds in Comm (Right y axis)', c = 'red', ha='center')
+fig.text(0.5, 0.03, 'Communities sorted acc. size', ha='center')
 
-ax.legend()
-ax.set_xlabel('No. Communities')
-ax.set_ylabel('% Belongingness')
-ax.set_title('BerkStan: n = 685K, m = 7.6M, mod = 0.94')
-
-plt.savefig('BerkStan_Comm.png', dpi = 500)
+plt.savefig('BerkStan_Comm_Grappolo.png', dpi = 500)
 
 
 
